5c7bcc3df2fe048c5c8a12ee7c969a8ada4aae39
Benoit Girard <bgirard@mozilla.com>
2013-04-30 19:02:20 -0400

    Parsing gfx

--- doxygen-mozilla.sh
+++ doxygen-mozilla.sh
@@ -2,10 +2,30 @@
 
 set -e
 
+[ hash dot ] || {
+  echo "Must install 'dot' first from graphviz"
+  exit
+}
+
 [ -e doxygen-svn ] || {
   svn co https://doxygen.svn.sourceforge.net/svnroot/doxygen/trunk doxygen-svn
 }
 
-cd doxygen-svn
-./configure
-make
+[ -e doxygen-svn/bin/doxygen ] || {
+  cd doxygen-svn
+  ./configure
+  make
+}
+
+[ -e tree ] || {
+  hg clone ssh://hg.mozilla.org/mozilla-central .
+  echo test
+}
+
+(
+  cd tree
+  hg pull
+)
+
+pwd
+./doxygen-svn/bin/doxygen doxygen.cfg
